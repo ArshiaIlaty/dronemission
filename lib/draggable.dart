09f5321483820490cd4dd9b54a8eb52f2aa61c05
Arshia Ilaty <71960053+ArshiaIlati@users.noreply.github.com>
2023-05-19 16:49:34 -0700

    Extention methods and widget + rating bar added

--- lib/draggable.dart
+++ lib/draggable.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+// learning extention methods and widget in order to make a custom change in the others' library
+extension CustomString on String {
+  get firstLetterAndSecondLetter {
+    return this[0] + this[1];
+  }
+}
+// This approach helps us to write more clear and readable code and prevent of repeating the same code
+extension MyExtention on Text{
+  Container addBox(){
+    return Container(
+      child: this,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
+//Learning about the draggable widget
 class DraggableWidget extends StatefulWidget {
   const DraggableWidget({Key? key}) : super(key: key);
 
   @override
   State<DraggableWidget> createState() => _DraggableWidgetState();
 }
-
 class _DraggableWidgetState extends State<DraggableWidget> {
   Color acceptedColor = Colors.grey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Draggable Widget'),
+        title: Text('Merged Widget', style: GoogleFonts.acme(),),
       ),
       body: SizedBox(
         height: double.infinity,
@@ -22,6 +44,28 @@ class _DraggableWidgetState extends State<DraggableWidget> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+
+// rating bar implementation
+            Center(
+              child: RatingBar(
+                ratingWidget: RatingWidget(
+                  full: const Icon(Icons.star, color: Colors.amber),
+                  half: const Icon(Icons.star_half, color: Colors.amber),
+                  empty: const Icon(Icons.star_border, color: Colors.amber),
+                ),
+                initialRating: 3,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                onRatingUpdate: (newRating) {
+                  setState(() {
+                    var rating = newRating;
+                  });
+                },
+              ),
+            ),
             Center(
               child: Draggable<Color>(
                 // pass the data to the drag target
@@ -53,17 +97,18 @@ class _DraggableWidgetState extends State<DraggableWidget> {
               },
               builder: (context, candidateData, rejectedData) {
                 return Container(
-                    height: 100,
-                    width: 100,
-                    color: acceptedColor,
-                    child: Center(
-                      child: Text(
-                        'Drag Here',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+                  height: 100,
+                  width: 100,
+                  color: acceptedColor,
+                  child: Center(
+                    child: Text(
+                      'Drag Here',
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
-                    ));
+                    ),
+                  ),
+                );
               },
             ),
           ],
