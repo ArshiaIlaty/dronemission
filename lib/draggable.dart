04f0792069440130b645556a659200b2934713ef
Arshia Ilaty <58371460+ArshiaIlaty@users.noreply.github.com>
2023-05-20 19:31:41 -0700

    Update router and navigation

--- lib/draggable.dart
+++ lib/draggable.dart
@@ -1,6 +1,25 @@
+import 'package:dronemission/slidebar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'clock_widget.dart';
+
+extension RouterContext on BuildContext {
+  //{} this means optional argument in this section, this helps us to send data for some of our navigations
+  toNamed(String routeName, {Object? arg}) {
+    Navigator.of(this).pushNamed(routeName, arguments: arg);
+  }
+  toNamedUntil(String routeName) {
+    Navigator.of(this).pushNamedAndRemoveUntil(routeName, (route) => false);
+  } 
+  toPushReplacement(Widget page) {
+    Navigator.of(this).pushReplacement(MaterialPageRoute(builder: (context) => page));
+  } 
+  // push(Widget page) {
+  //   Navigator.of(this).push(MaterialPageRoute(builder: (context) => page));
+  // }
+}
 
 // learning extention methods and widget in order to make a custom change in the others' library
 extension CustomString on String {
@@ -8,9 +27,10 @@ extension CustomString on String {
     return this[0] + this[1];
   }
 }
+
 // This approach helps us to write more clear and readable code and prevent of repeating the same code
-extension MyExtention on Text{
-  Container addBox(){
+extension MyExtention on Text {
+  Container addBox() {
     return Container(
       child: this,
       padding: EdgeInsets.all(10),
@@ -22,6 +42,7 @@ extension MyExtention on Text{
     );
   }
 }
+
 //Learning about the draggable widget
 class DraggableWidget extends StatefulWidget {
   const DraggableWidget({Key? key}) : super(key: key);
@@ -29,87 +50,103 @@ class DraggableWidget extends StatefulWidget {
   @override
   State<DraggableWidget> createState() => _DraggableWidgetState();
 }
+
 class _DraggableWidgetState extends State<DraggableWidget> {
   Color acceptedColor = Colors.grey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Merged Widget', style: GoogleFonts.acme(),),
+        title: Text(
+          'Merged Widget',
+          style: GoogleFonts.acme(),
+        ),
       ),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Container(
+        child: Row(
           children: [
-
-// rating bar implementation
-            Center(
-              child: RatingBar(
-                ratingWidget: RatingWidget(
-                  full: const Icon(Icons.star, color: Colors.amber),
-                  half: const Icon(Icons.star_half, color: Colors.amber),
-                  empty: const Icon(Icons.star_border, color: Colors.amber),
-                ),
-                initialRating: 3,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
-                onRatingUpdate: (newRating) {
-                  setState(() {
-                    var rating = newRating;
-                  });
-                },
-              ),
+            Column(
+              children: [
+                SizedBox(height: 100, width: 200, child: ClockWidget()),
+                SizedBox(height: 100, width: 500, child: SliderExample()),
+              ],
             ),
-            Center(
-              child: Draggable<Color>(
-                // pass the data to the drag target
-                data: Colors.blue,
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                ),
-                // when you drag the widget, you can see the feedback widget
-                feedback: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue.withOpacity(0.5),
-                ),
-                childWhenDragging: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            DragTarget<Color>(
-              onWillAccept: (value) => true,
-              onAccept: (value) {
-                setState(() {
-                  acceptedColor = value;
-                });
-              },
-              builder: (context, candidateData, rejectedData) {
-                return Container(
-                  height: 100,
-                  width: 100,
-                  color: acceptedColor,
-                  child: Center(
-                    child: Text(
-                      'Drag Here',
-                      style: TextStyle(
-                        color: Colors.white,
+            SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // rating bar implementation
+                  Center(
+                    child: RatingBar(
+                      ratingWidget: RatingWidget(
+                        full: const Icon(Icons.star, color: Colors.amber),
+                        half: const Icon(Icons.star_half, color: Colors.amber),
+                        empty:
+                            const Icon(Icons.star_border, color: Colors.amber),
+                      ),
+                      initialRating: 3,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      onRatingUpdate: (newRating) {
+                        setState(() {
+                          var rating = newRating;
+                        });
+                      },
+                    ),
+                  ),
+                  Center(
+                    child: Draggable<Color>(
+                      // pass the data to the drag target
+                      data: Colors.blue,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.blue,
+                      ),
+                      // when you drag the widget, you can see the feedback widget
+                      feedback: Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.blue.withOpacity(0.5),
+                      ),
+                      childWhenDragging: Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                );
-              },
+                  DragTarget<Color>(
+                    onWillAccept: (value) => true,
+                    onAccept: (value) {
+                      setState(() {
+                        acceptedColor = value;
+                      });
+                    },
+                    builder: (context, candidateData, rejectedData) {
+                      return Container(
+                        height: 100,
+                        width: 100,
+                        color: acceptedColor,
+                        child: Center(
+                          child: Text(
+                            'Drag Here',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
